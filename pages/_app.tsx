d845368f933def02980fcb6faa01834d30b82b04
Hai Nguyen Van <hai-nguyen@cybozu.vn>
2022-02-13 07:49:03 +0700

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
+import SideBar from '../components/SideBar';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="grid grid-cols-12 gap-6">
-      <div className="col-span-3 bg-white">
-        Sidebar
+    <div className="grid grid-cols-12 gap-6 px-5 lg:px-48 my-14">
+      <div className="col-span-12 bg-white lg:col-span-3 rounded-2xl p-4 text-center">
+        <SideBar/>
       </div>
-      <div className="col-span-9 bg-white">
+      <div className="col-span-12 bg-white lg:col-span-9 rounded-2xl">
         <Component {...pageProps} />
       </div>
     </div>
